3f585519ed808e2baa13336a5237efae780b7831
Angel Guevara, Jr <aguevara.716@gmail.com>
2020-10-22 09:54:37 -0400

    Revised documentation

--- XamarinCommunityToolkit/Helpers/BindablePropertyBuilder.shared.cs
+++ XamarinCommunityToolkit/Helpers/BindablePropertyBuilder.shared.cs
@@ -37,7 +37,7 @@ namespace Xamarin.CommunityToolkit.Helpers
 		/// property that is bound to the <see cref="BindableProperty"/>
 		/// </summary>
 		/// <param name="propertyName">The name of the property that is bound to the <see cref="BindableProperty"/></param>
-		/// <returns>The <see cref="BindablePropertyBuilder"/> to support the fluent syntax</returns>
+		/// <returns>The same <see cref="BindablePropertyBuilder"/> to support the fluent syntax</returns>
 		public BindablePropertyBuilder SetPropertyName(string propertyName)
 		{
 			this.propertyName = propertyName;
@@ -48,7 +48,7 @@ namespace Xamarin.CommunityToolkit.Helpers
 		/// (Required) Sets the type of the value that is bound to the <see cref="BindableProperty"/>
 		/// </summary>
 		/// <param name="returnType">The type of the value that is bound to the <see cref="BindableProperty"/></param>
-		/// <returns>The <see cref="BindablePropertyBuilder"/> to support the fluent syntax</returns>
+		/// <returns>The same <see cref="BindablePropertyBuilder"/> to support the fluent syntax</returns>
 		public BindablePropertyBuilder SetReturnType(Type returnType)
 		{
 			this.returnType = returnType;
@@ -59,14 +59,14 @@ namespace Xamarin.CommunityToolkit.Helpers
 		/// (Required) Sets the type of the value that is bound to the <see cref="BindableProperty"/>
 		/// </summary>
 		/// <typeparam name="TReturn">The type of the value that is bound to the <see cref="BindableProperty"/></typeparam>
-		/// <returns>The <see cref="BindablePropertyBuilder"/> to suport the fluent syntax</returns>
+		/// <returns>The same <see cref="BindablePropertyBuilder"/> to suport the fluent syntax</returns>
 		public BindablePropertyBuilder SetReturnType<TReturn>() => SetReturnType(typeof(TReturn));
 
 		/// <summary>
 		/// Sets the property's default value
 		/// </summary>
 		/// <param name="defaultValue">The property's default value</param>
-		/// <returns>The <see cref="BindablePropertyBuilder"/> to support the fluent syntax</returns>
+		/// <returns>The same <see cref="BindablePropertyBuilder"/> to support the fluent syntax</returns>
 		public BindablePropertyBuilder SetDefaultValue(object defaultValue)
 		{
 			this.defaultValue = defaultValue;
@@ -77,7 +77,7 @@ namespace Xamarin.CommunityToolkit.Helpers
 		/// Sets the property's default <see cref="BindingMode"/>
 		/// </summary>
 		/// <param name="defaultBindingMode">The property's default <see cref="BindingMode"/></param>
-		/// <returns>The <see cref="BindablePropertyBuilder"/> to support the fluent syntax</returns>
+		/// <returns>The same <see cref="BindablePropertyBuilder"/> to support the fluent syntax</returns>
 		public BindablePropertyBuilder SetDefaultBindingMode(BindingMode defaultBindingMode)
 		{
 			this.defaultBindingMode = defaultBindingMode;
@@ -88,7 +88,7 @@ namespace Xamarin.CommunityToolkit.Helpers
 		/// (Required) Sets the type (usually a control) that's declaring the <see cref="BindableProperty"/>
 		/// </summary>
 		/// <param name="declaringType">The type (usually a control) that's declaring the <see cref="BindableProperty"/></param>
-		/// <returns>The <see cref="BindablePropertyBuilder"/> to support the fluent syntax</returns>
+		/// <returns>The same <see cref="BindablePropertyBuilder"/> to support the fluent syntax</returns>
 		public BindablePropertyBuilder SetDeclaringType(Type declaringType)
 		{
 			this.declaringType = declaringType;
@@ -99,14 +99,14 @@ namespace Xamarin.CommunityToolkit.Helpers
 		/// (Required) Sets the type (usually a control) that's declaring the <see cref="BindableProperty"/>
 		/// </summary>
 		/// <typeparam name="TDeclaring">The type (usually a control) that's declaring the <see cref="BindableProperty"/></typeparam>
-		/// <returns>The <see cref="BindablePropertyBuilder"/> to support the fluent syntax</returns>
+		/// <returns>The same <see cref="BindablePropertyBuilder"/> to support the fluent syntax</returns>
 		public BindablePropertyBuilder SetDeclaringType<TDeclaring>() => SetDeclaringType(typeof(TDeclaring));
 
 		/// <summary>
 		/// Sets the delegate that runs when a bound value is set.
 		/// </summary>
 		/// <param name="validateValueDelegate">The delegate that runs when a bound value is set.</param>
-		/// <returns>The <see cref="BindablePropertyBuilder"/> to support the fluent syntax</returns>
+		/// <returns>The same <see cref="BindablePropertyBuilder"/> to support the fluent syntax</returns>
 		public BindablePropertyBuilder SetValidateValueDelegate(BindableProperty.ValidateValueDelegate validateValueDelegate)
 		{
 			this.validateValueDelegate = validateValueDelegate;
@@ -117,7 +117,7 @@ namespace Xamarin.CommunityToolkit.Helpers
 		/// Sets the delegate that runs after a bound value has changed.
 		/// </summary>
 		/// <param name="bindingPropertyChangedDelegate">The delegate that runs after a bound value has changed.</param>
-		/// <returns>The <see cref="BindablePropertyBuilder"/> to support the fluent syntax</returns>
+		/// <returns>The same <see cref="BindablePropertyBuilder"/> to support the fluent syntax</returns>
 		public BindablePropertyBuilder SetPropertyChangedDelegate(BindableProperty.BindingPropertyChangedDelegate bindingPropertyChangedDelegate)
 		{
 			this.bindingPropertyChangedDelegate = bindingPropertyChangedDelegate;
@@ -128,7 +128,7 @@ namespace Xamarin.CommunityToolkit.Helpers
 		/// Sets the delegate that runs before a bound value is changed.
 		/// </summary>
 		/// <param name="bindingPropertyChangingDelegate">The delegate that runs before a bound value is changed.</param>
-		/// <returns>The <see cref="BindablePropertyBuilder"/> to support the fluent syntax</returns>
+		/// <returns>The same <see cref="BindablePropertyBuilder"/> to support the fluent syntax</returns>
 		public BindablePropertyBuilder SetPropertyChangingDelegate(BindableProperty.BindingPropertyChangingDelegate bindingPropertyChangingDelegate)
 		{
 			this.bindingPropertyChangingDelegate = bindingPropertyChangingDelegate;
@@ -139,7 +139,7 @@ namespace Xamarin.CommunityToolkit.Helpers
 		/// Sets the delegate used to coerce the range of a value.
 		/// </summary>
 		/// <param name="coerceValueDelegate">A delegate used to coerce the range of a value.</param>
-		/// <returns>The <see cref="BindablePropertyBuilder"/> to support the fluent syntax</returns>
+		/// <returns>The same <see cref="BindablePropertyBuilder"/> to support the fluent syntax</returns>
 		public BindablePropertyBuilder SetCoerceValueDelegate(BindableProperty.CoerceValueDelegate coerceValueDelegate)
 		{
 			this.coerceValueDelegate = coerceValueDelegate;
@@ -150,7 +150,7 @@ namespace Xamarin.CommunityToolkit.Helpers
 		/// Sets the delegate used to initialize the default value for reference types.
 		/// </summary>
 		/// <param name="createDefaultValueDelegate">The delegate used to initialize the default value for reference types.</param>
-		/// <returns>The <see cref="BindablePropertyBuilder"/> to support the fluent syntax</returns>
+		/// <returns>The same <see cref="BindablePropertyBuilder"/> to support the fluent syntax</returns>
 		public BindablePropertyBuilder SetCreateDefaultValueDelegate(BindableProperty.CreateDefaultValueDelegate createDefaultValueDelegate)
 		{
 			this.createDefaultValueDelegate = createDefaultValueDelegate;
